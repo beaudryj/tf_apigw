494e292bc8af599447bace5fd448a5ba1035fd5c
jbeaudry <joe.beaudry@slalom.com>
2021-09-13 23:58:31 -0400

    removing tags

--- apigw.tf
+++ apigw.tf
@@ -1,7 +1,6 @@
 resource "aws_api_gateway_rest_api" "api" {
   name                     = var.api_name
   description              = var.api_description
-  tags                     = var.tags
   minimum_compression_size = var.minimum_compression_size
 
   endpoint_configuration {
